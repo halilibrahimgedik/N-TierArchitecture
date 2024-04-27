2cee38123b44da344f90805d6a69cca9a34f6856
Halil Gedik <halilgedik4234@gmail.com>
2024-04-27 07:25:54 +0300

    Created CustomResponseDto class.

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        // istek yapılan endpoint'den zaten durum kodu gelicek. Bu yüzden int StatusCode'un geri dönülmesine gerek yok.
+        // Sadece kod içinde lazım olacak bize.
+        [JsonIgnore]
+        public int StatusCode { get; set; } 
+
+        public List<string> Errors { get; set; }
+
+
+        // Başarılı Durum Metotları
+        public static CustomResponseDto<T> Success(int statusCode,T data)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Data = data };
+        }
+
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode };
+        }
+
+
+        // Başarısız Durum Metotları
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors) 
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode , Errors = errors };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+    }
+}
